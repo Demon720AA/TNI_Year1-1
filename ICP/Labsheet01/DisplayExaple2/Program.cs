426c6bd57187c4ee62ef6aa0a70a0605cf32bc2d
Supanut Arun <139570148+Demon720AA@users.noreply.github.com>
2025-06-24 16:40:59 +0700

    LAB1.4

---
+++ ICP/Labsheet01/DisplayExaple2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DisplayExaple2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---DisplaExample1---");
+            Console.Write("Hello, my name is Supanut Arun\n");
+            Console.WriteLine("I'am study at Thai-Nichi Institute of Technology\n");
+            Console.WriteLine("\tNice to meet you!!");
+
+            Console.ReadKey();
+        }
+    }
+}
