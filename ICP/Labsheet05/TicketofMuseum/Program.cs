196525d75cf0e08c738f8b61c92d10fb8ff7f51d
Demon720AA <aeaey.supanuth@gmail.com>
2025-07-23 20:39:20 +0700

    Refactor TicketofMuseum to Conditional Operator

--- ICP/Labsheet05/TicketofMuseum/Program.cs
+++ ICP/Labsheet05/TicketofMuseum/Program.cs
@@ -11,19 +11,23 @@
             Console.WriteLine("--------------------------------------");
             int thisYear = DateTime.Today.Year, age = thisYear - birthYear;
             double price, discount = 0;
-            if (age >= 18)
-            {
-                price = 150;
-            }
-            else
-            {
-                price = 100;
-            }
-            if (member == 'Y' || member == 'y')
-            {
-                discount = 5;
-                Console.WriteLine($"Get discount\t{discount,10} %");
-            }
+            price = (age >= 18) ? 150 : 100;
+            // if (age >= 18)
+            // {
+            //     price = 150;
+            // }
+            // else
+            // {
+            //     price = 100;
+            // }
+            discount = (member == 'Y' || member == 'y') ? 5 : 0;
+            Console.Write($"" + (member == 'Y' || member == 'y') ?"Get discount\t{discount,10} %" : "");
+            //(member == 'Y' || member == 'y') ? Console.WriteLine($"Get discount\t{discount,10} %") : Console.Write("");  
+            //if (member == 'Y' || member == 'y')
+            //{
+            //    discount = 5;
+            //    Console.WriteLine($"Get discount\t{discount,10} %");
+            //}
             price -= (price * (discount / 100));
             Console.WriteLine($"Total price\t{price,10:N2} baht.");
             Console.ReadKey();
