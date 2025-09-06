22adc4b184a345020fe652716d36de13deaa9b98
Supanut Arun <139570148+Demon720AA@users.noreply.github.com>
2025-09-06 21:32:43 +0700

    Add files via upload

---
+++ ICP/Labsheet09/Method03_ExamResult/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Method03_ExamResult
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input mid-term score : ");
+            int mid = int.Parse(Console.ReadLine());
+            Console.Write("Input final    score : ");
+            int final = int.Parse(Console.ReadLine());
+
+            sumScore(mid, final);
+            Console.ReadKey();
+        }
+        static void sumScore(int mid, int final)
+        {
+            Console.WriteLine();
+            gradeResult(mid+final);
+        }
+        static void gradeResult(int sum)
+        {
+            Console.WriteLine("Your score is "+ (sum >= 50) ? "PASS" : "FAIL", $"({sum})");
+        }
+    }
+}
