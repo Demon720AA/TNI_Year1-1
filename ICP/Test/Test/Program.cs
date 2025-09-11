9a449faede34aa2ea1a6efe48a40bc4d74c1ffbc
Demon720AA <aeaey.supanuth@gmail.com>
2025-09-12 06:02:18 +0700

    Add Lab sheet10 and PDF

---
+++ ICP/Test/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password, realpwd = "123";
+            int count = 0;
+            do
+            {
+                Console.Write("Input Password");
+                if (count == 3) break;
+                password = Console.ReadLine();
+                count++;
+            } while (password != realpwd);
+
+
+            Console.ReadKey();
+        }
+    }
+}
